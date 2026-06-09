2caef15f84d2e3a43adc97f5665ea545d3776fa6
albrecht912 <syaqeerzainal@gmail.com>
2026-06-09 12:39:31 +0800

    update auto logout to 10 minutes. password expiry to 2 minutes

--- config/auth.php
+++ config/auth.php
@@ -94,7 +94,7 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 30,
+            'expire' => 2,
             'throttle' => 60,
         ],
     ],
@@ -110,6 +110,6 @@ return [
     |
     */
 
-    'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 1800),
+    'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 120),
 
 ];
